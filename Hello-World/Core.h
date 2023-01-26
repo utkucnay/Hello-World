c8c401bb8f59d0c3fd4b1d7377ec8d52482f974f
Utkucan AY <utkucnay@gmail.com>
2023-01-26 21:39:31 +0300

    Init

---
+++ Hello-World/Core.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <array>
+#include <algorithm>
+#include <string>
+#include <memory>
---
+++ Hello-World/Person.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Core.h"
+
+class Person
+{
+public:
+	Person();
+
+
+
+public:
+	std::string name;
+	float age;
+};
+
